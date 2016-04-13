84a103e828cddf88221fa1294bb793ea3800b1d3
bgrove1974 <bgroveben@gmail.com>
2016-04-13 16:42:30 -0700

    jQuery Selectors

---
+++ module-5/js/jQuerySelectors.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+// Run the following commands in the console:
+  $('*').length;
+  $('*:empty').length;
+  $('p').length;
+  $('.blueBg').length;
+  $('.redBg').length;
+  $('article.redBg').length;
+  $('#test1').length;
+  $('article[name=topFirst]').length;
+  $('article[name!=top]').length;
+  $('article[name^=top]').length;
+  $('article[name^=bottom]').length;
+  $('article[name$=Second]').length;
+  $('footer > p').length;
+  $('article[class]').length;
+  $('article[id]').length;
+});
