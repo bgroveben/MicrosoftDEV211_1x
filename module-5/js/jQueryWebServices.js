9d2034d4438199ef1b76f603417d861cdb197399
bgrove1974 <bgroveben@gmail.com>
2016-04-14 00:16:25 -0700

    Calling Web Services Using jQuery: Part 2

--- module-5/js/jQueryWebServices.js
+++ module-5/js/jQueryWebServices.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   makeRequests();
 });
 
+/* Uncomment and run this one first:
+
 function makeRequests() {
   $.ajax({
     // url: 'http://www.example.com/api',  #! this url throws an error
@@ -17,4 +19,32 @@ function makeRequests() {
   .fail(function() {
     alert('An error has occurred')
   });
+}
+*/
+
+/* Uncomment and run this one second:
+
+function makeRequests() {
+  $.get({
+    url: 'http://httpbin.org/user-agent',
+    dataType: 'json'
+  })
+  .done(function(response) {
+    alert(JSON.stringify(response));
+    $('body > p').text(
+      response['user-agent']
+    );
+  });
+}
+*/
+
+/* Run this one third/last */
+function makeRequests() {
+  $.getJSON('http://httpbin.org/ip')
+    .done(function(response) {
+      alert(JSON.stringify(response));
+      $('body > p').text(
+        response.origin
+      );
+    });
 }
